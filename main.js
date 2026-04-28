87b91fa07adac936b5fc5cd99381fd9a2da9f94e
taleah <taleahelbanna796@gmail.com>
2026-04-27 20:05:21 -0700

    e
    e

--- main.js
+++ main.js
@@ -22,12 +22,15 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-Ball.prototype.draw = function() {
+Ball.prototype.draw = function() {  
   ctx.beginPath();
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx;fill();
 }
+
+let testBall = new Ball (50, 100, 4, 4, 'blue', 10);
+testBall.draw()
 
 Ball.prototype.update = function() {
   if ((this.x + this.size) >= width) {
@@ -48,6 +51,20 @@ Ball.prototype.update = function() {
 
   this.x += this.velX;
   this.y += this.velY;
+}
+
+Ball.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')';
+      }
+    }
+  }
 }
 
 let balls = [];
@@ -78,23 +95,3 @@ function loop() {
 
   requestAnimationFrame(loop);
 }
-
-Ball.prototype.collisionDetect = function() {
-  for (let j = 0; j < balls.length; j++) {
-    if (!(this === balls[j])) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
-      }
-    }
-  }
-}
-
-balls[i].update(); line:
-balls[i].collisionDetect();
-
-ball.prototype.update; 
-{balls[i].update()+}
